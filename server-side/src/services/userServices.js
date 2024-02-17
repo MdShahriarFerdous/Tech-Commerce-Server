3bd0b1505711f3b483025d2c64fd2f5477782623
Ferdous <109579505+MdShahriarFerdous@users.noreply.github.com>
2024-02-17 22:01:55 +0600

    Update userServices.js

--- server-side/src/services/userServices.js
+++ server-side/src/services/userServices.js
@@ -267,43 +267,34 @@ exports.UserLoginService = async (req, res) => {
 		);
 
 
-		// res.cookie("accessToken", accessToken, {
-		// 	expires: new Date(Date.now() + 1000 * 60 * 10), //10 minutes
-		// 	httpOnly: true,
-		// 	path: "/",
-		// 	secure: true,
-		// 	sameSite: "Lax",
-		// });
+		res.cookie("accessToken", accessToken, {
+			expires: new Date(Date.now() + 1000 * 60 * 10), //10 minutes
+			httpOnly: true,
+			path: "/",
+			secure: true,
+			sameSite: "Lax",
+		});
 
 		// Set access token cookie
-		const accessTokenCookie = `accessToken=${accessToken}; Path=/; Expires=${new Date(
-			Date.now() + 1000 * 60 * 10
-		).toUTCString()}; HttpOnly; Secure; SameSite=Lax`;
-		res.setHeader("Set-Cookie", accessTokenCookie);
+		// const accessTokenCookie = `accessToken=${accessToken}; Path=/; Expires=${new Date(
+		// 	Date.now() + 1000 * 60 * 10
+		// ).toUTCString()}; HttpOnly; Secure; SameSite=Lax`;
+		// res.setHeader("Set-Cookie", accessTokenCookie);
 
 		// Set refresh token cookie
-		const refreshTokenCookie = `refreshToken=${refreshToken}; Path=/; Expires=${new Date(
-			Date.now() + 1000 * 60 * 60 * 12
-		).toUTCString()}; HttpOnly; Secure; SameSite=Lax`;
-		res.setHeader("Set-Cookie", refreshTokenCookie);
+		// const refreshTokenCookie = `refreshToken=${refreshToken}; Path=/; Expires=${new Date(
+		// 	Date.now() + 1000 * 60 * 60 * 12
+		// ).toUTCString()}; HttpOnly; Secure; SameSite=Lax`;
+		// res.setHeader("Set-Cookie", refreshTokenCookie);
 
-		// res.cookie("refreshToken", refreshToken, {
-		// 	expires: new Date(Date.now() + 1000 * 60 * 60 * 12), //12 hr
-		// 	httpOnly: true,
-		// 	path: "/",
-		// 	secure: true,
-		// 	sameSite: "Lax",
-		// });
+		res.cookie("refreshToken", refreshToken, {
+			expires: new Date(Date.now() + 1000 * 60 * 60 * 12), //12 hr
+			httpOnly: true,
+			path: "/",
+			secure: true,
+			sameSite: "Lax",
+		});
 
-		// Set access token cookie
-		// const accessTokenCookie = `accessToken=${accessToken}; Path=/; Expires=${new Date(Date.now() + 1000 * 60 * 10).toUTCString()}; HttpOnly; Secure; SameSite=Lax`;
-		// res.setHeader('Set-Cookie', accessTokenCookie);
-
-
-
-			// Set refresh token cookie
-		// const refreshTokenCookie = `refreshToken=${refreshToken}; Path=/; Expires=${new Date(Date.now() + 1000 * 60 * 60 * 12).toUTCString()}; HttpOnly; Secure; SameSite=Lax`;
-		// res.setHeader('Set-Cookie', refreshTokenCookie);
 
 		const user = {
 			username: databaseUser.username,
