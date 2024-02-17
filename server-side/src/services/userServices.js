fabf28aa58de8e790c746cc9b3c63fc78ab3a118
Ferdous <109579505+MdShahriarFerdous@users.noreply.github.com>
2024-02-17 19:36:09 +0600

    Update userServices.js

--- server-side/src/services/userServices.js
+++ server-side/src/services/userServices.js
@@ -270,6 +270,7 @@ exports.UserLoginService = async (req, res) => {
 			expires: new Date(Date.now() + 1000 * 60 * 10), //10 minutes
 			httpOnly: true,
 			path: "/",
+			domain: "https://plainb-tech-commerce-pied.vercel.app",
 			secure: true,
 			sameSite: "Strict",
 		});
@@ -278,6 +279,7 @@ exports.UserLoginService = async (req, res) => {
 			expires: new Date(Date.now() + 1000 * 60 * 60 * 12), //12 hr
 			httpOnly: true,
 			path: "/",
+			domain: "https://plainb-tech-commerce-pied.vercel.app",
 			secure: true,
 			sameSite: "Strict",
 		});
