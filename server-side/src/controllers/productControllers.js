a453b2f9ab8c3a1744e8a3ba44a6cf882c5a57da
MHRSRoni <132211823+MHRSRoni@users.noreply.github.com>
2024-02-19 19:58:50 +0600

    Update productControllers.js

--- server-side/src/controllers/productControllers.js
+++ server-side/src/controllers/productControllers.js
@@ -86,7 +86,9 @@ exports.createBrand = async (req, res, next) => {
 //client--> data.payload.brandLists = []
 exports.productBrandList = async (req, res, next) => {
 	try {
+		console.log("in the controller")
 		const brandLists = await BrandListService();
+		console.log(brandLists)
 
 		return successResponse(res, {
 			statusCode: 200,
