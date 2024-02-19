a6b15088ebc3fd2ad336a037906a3843c061f03c
MHRSRoni <132211823+MHRSRoni@users.noreply.github.com>
2024-02-19 19:35:43 +0600

    testing api

--- server-side/src/routes/featureRoutes.js
+++ server-side/src/routes/featureRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { featureList } = require("../controllers/featureControllers");
 const router = express.Router();
 
-router.get("/get-feature-list", featureList);
+router.get("/get-feature-list", (req, res) => {
+  res.json({success : true})
+});
 
 module.exports = router;
